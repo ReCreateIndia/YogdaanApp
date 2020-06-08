89126a3c61a3a1a198733e5837ee6e204a51f476
Vishnu Bhat <vishnubhat820@gmail.com>
2020-06-08 21:48:32 +0530

    Resend OTP btn added and updated Login and Otp Activities

--- app/src/main/java/recreate/india/yogdaan/LoginActivity.java
+++ app/src/main/java/recreate/india/yogdaan/LoginActivity.java
@@ -25,13 +25,11 @@ import com.google.firebase.auth.PhoneAuthProvider;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
+
 public class LoginActivity extends AppCompatActivity {
     private EditText phone_number_edittext;
     private Button create_btn;
     private ProgressBar log_progress;
-    private FirebaseAuth mAuth;
-    private FirebaseUser mCurrentUser;
-    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
 
     @Override
@@ -41,82 +39,34 @@ public class LoginActivity extends AppCompatActivity {
         phone_number_edittext = findViewById(R.id.phone_number_text);
         create_btn = findViewById(R.id.generate_btn);
         log_progress = findViewById(R.id.login_progress);
-        mAuth = FirebaseAuth.getInstance();
-        mCurrentUser = mAuth.getCurrentUser();
+
 
         create_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String phone_number = phone_number_edittext.getText().toString();
                 String code = "+91";
-                String complete_phone_number = code + phone_number;
+                final String complete_phone_number = code + phone_number;
                 if (phone_number.length() == 10) {
                     create_btn.setEnabled(false);
                     log_progress.setVisibility(View.VISIBLE);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    Intent otpIntent = new Intent(LoginActivity.this, OtpActivity.class);
+                                    otpIntent.putExtra("phone_number", complete_phone_number);
+                                    startActivity(otpIntent);
+                                }
+                            },
+                            1500);
 
-                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                            complete_phone_number,
-                            120,
-                            TimeUnit.SECONDS,
-                            LoginActivity.this,
-                            mCallbacks
-                    );
 
-                }else{
+                } else {
 
-                    Toast.makeText(LoginActivity.this, "Please enter a valid number.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Please enter a valid number.", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
-        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-            @Override
-            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-                signInWithPhoneAuthCredential(phoneAuthCredential);
-            }
-
-            @Override
-            public void onVerificationFailed(FirebaseException e) {
-
-                log_progress.setVisibility(View.INVISIBLE);
-                create_btn.setEnabled(true);
-                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onCodeSent(@NonNull final String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                super.onCodeSent(s, forceResendingToken);
-
-                new android.os.Handler().postDelayed(
-                        new Runnable() {
-                            public void run() {
-                                Intent otpIntent = new Intent(LoginActivity.this, OtpActivity.class);
-                                otpIntent.putExtra("AuthCredentials", s);
-                                startActivity(otpIntent);
-                            }
-                        },
-                        5000);
-            }
-        };
-    }
-    private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
-
-        mAuth.signInWithCredential(phoneAuthCredential)
-                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Intent homeIntent = new Intent(LoginActivity.this, MainActivity.class);
-                            homeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(homeIntent);
-                            // ...
-                        } else {
-                            Toast.makeText(LoginActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                        log_progress.setVisibility(View.INVISIBLE);
-                        create_btn.setEnabled(true);
-                    }
-                });
     }
 }
--- app/src/main/java/recreate/india/yogdaan/OtpActivity.java
+++ app/src/main/java/recreate/india/yogdaan/OtpActivity.java
@@ -13,45 +13,148 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
+import java.util.concurrent.TimeUnit;
+
 public class OtpActivity extends AppCompatActivity {
     private Button otp_btn;
     private ProgressBar otp_progressbar;
     private FirebaseAuth mAuth;
     private EditText otp;
-    private String mAuthVerificationId;
+    private String mPhoneNumber;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private Button resendBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
         otp_btn = findViewById(R.id.verify_btn);
-        mAuthVerificationId = getIntent().getStringExtra("AuthCredentials");
+
+        mPhoneNumber = getIntent().getStringExtra("phone_number");
         otp_progressbar = findViewById(R.id.otp_progress);
         otp = findViewById(R.id.otp_text_view);
         mAuth = FirebaseAuth.getInstance();
+        sendVerificationCode(mPhoneNumber);
+        resendBtn = findViewById(R.id.resend_btn);
+
+
+
+        resendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                otp_progressbar.setVisibility(View.VISIBLE);
+                new android.os.Handler().postDelayed(
+                        new Runnable() {
+                            public void run() {
+                                resendVerificationCode(mPhoneNumber, mResendToken);
+                            }
+                        },
+                        1500);
+            }
+        });
         otp_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String otpText = otp.getText().toString();
-                if (otpText.isEmpty()){
+                String code = otp.getText().toString().trim();
+                if (code.length() != 6) {
 
-                    Toast.makeText(OtpActivity.this, "Please fill in the form and try again.",Toast.LENGTH_SHORT).show();
-                }
-                else{
+                    Toast.makeText(OtpActivity.this, "Please fill in the form and try again.", Toast.LENGTH_SHORT).show();
+                } else {
                     otp_progressbar.setVisibility(View.VISIBLE);
                     otp_btn.setEnabled(false);
-                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mAuthVerificationId, otpText);
-                    signInWithPhoneAuthCredential(credential);
+                    verifyVerificationCode(code);
+
 
                 }
 
             }
         });
+
+    }
+
+    private void resendVerificationCode(String mPhoneNumber, PhoneAuthProvider.ForceResendingToken mResendToken) {
+        otp_progressbar.setVisibility(View.INVISIBLE);
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                mPhoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                OtpActivity.this,               // Activity (for callback binding)
+                mCallbacks,         // OnVerificationStateChangedCallbacks
+                mResendToken);             // ForceResendingToken from callbacks
+    }
+
+    private void sendVerificationCode(String mPhoneNumber) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                mPhoneNumber,
+                60,
+                TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
+                mCallbacks);
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            //Getting the code sent by SMS
+            final String code = phoneAuthCredential.getSmsCode();
+
+            //sometime the code is not detected automatically
+            //in this case the code will be null
+            //so user has to manually enter the code
+            if (code != null) {
+                otp.setText(code);
+                //verifying the code
+                otp_progressbar.setVisibility(View.VISIBLE);
+                new android.os.Handler().postDelayed(
+                        new Runnable() {
+                            public void run() {
+                                verifyVerificationCode(code);
+                            }
+                        },
+                        1000);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            otp_progressbar.setVisibility(View.INVISIBLE);
+            otp_btn.setEnabled(true);
+            Toast.makeText(OtpActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            //storing the verification id that is sent to the user
+            mVerificationId = s;
+            mResendToken = forceResendingToken;
+
+        }
+    };
+
+    private void gotoHomeActivity() {
+        Intent homeIntent = new Intent(OtpActivity.this, MainActivity.class);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(homeIntent);
+    }
+
+    private void verifyVerificationCode(String code) {
+        //creating the credential
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, code);
+
+        //signing the user
+        signInWithPhoneAuthCredential(credential);
 
     }
 
@@ -61,15 +164,12 @@ public class OtpActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Intent homeIntent = new Intent(OtpActivity.this, MainActivity.class);
-                            homeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(homeIntent);
+                            gotoHomeActivity();
                             // ...
                         } else {
-                           Toast.makeText(OtpActivity.this, task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            Toast.makeText(OtpActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
-                        otp_progressbar.setVisibility(View.INVISIBLE);
-                        otp_btn.setEnabled(true);
+
                     }
                 });
 
