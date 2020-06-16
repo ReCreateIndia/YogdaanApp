f90da8074490a24fe481a860558d2b9ee918d684
Anubhav <18ucs066@lnmiit.ac.in>
2020-06-16 11:26:09 +0530

    yes

---
+++ app/src/main/java/recreate/india/yogdaan/OurHelpers.java
@@ -0,0 +1,14 @@
+package recreate.india.yogdaan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OurHelpers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_our_helpers);
+    }
+}
